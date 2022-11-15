c34ce0f14f87d7e26020eb4213736710c15d9383
Tatiana Marina <marina.tatiana.f@gmail.com>
2022-11-15 23:23:21 +0300

    First commit

---
+++ script.py
@@ -0,0 +1,29 @@
+def read_codon_table():
+    path_to_file = 'data/codon_table.csv'
+    
+    with open(path_to_file, 'rt', encoding='utf8') as codon_table:
+        contents = codon_table.readlines()
+        
+    codon_dict = {}
+    
+    for line in contents:
+        codon = line.split(',')
+        codon_dict[codon[0]] = codon[2].replace('Stop', '.')
+    
+    return codon_dict
+
+CODON_DICT = read_codon_table()
+
+def convert_dna_to_rna(string):
+    return str(string).replace("T", "U")
+
+def convert_rna_to_protein(string):
+    
+    protein = []
+    for i in range(0, len(string), 3):
+        if len(string[i:i + 3]) != 3:
+            break
+        triplet = string[i:i + 3]
+        amino_acid = CODON_DICT[triplet]
+        protein.append(amino_acid)
+    return ''.join(protein)
