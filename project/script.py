2fbd989c361026dee56d309e473352eec426ca22
Tatiana Marina <marina.tatiana.f@gmail.com>
2022-11-28 00:00:56 +0300

    Added function to plot GC-ratio

--- project/script.py
+++ project/script.py
@@ -1,4 +1,5 @@
-from data_io import read_codon_table, read_codon_db, read_dna_rna_db
+from data_io import read_codon_table, read_codon_db, read_dna_rna_db, read_genome
+import matplotlib.pyplot as plt 
 
 #CODON_DICT = read_codon_table()
 CODON_DICT = read_codon_db()
@@ -22,3 +23,28 @@ def convert_rna_to_protein(string):
         amino_acid = CODON_DICT[triplet]
         protein.append(amino_acid)
     return ''.join(protein)
+
+
+def plot_genome(string, step=100, output_file='image/gc_ratio.png'):
+    x = []
+    y = []
+    for i in range(0, len(string), step):
+        block = string[i:i + step]
+        if len(block) != step:
+            break
+        g_count = block.count('G')
+        c_count = block.count('C')
+        gc_ratio = (g_count + c_count) * 100 / step
+        x.append(i)
+        y.append(gc_ratio)
+        
+    # plotting the points  
+    plt.plot(x, y)
+    # naming the x axis 
+    plt.xlabel('genome position') 
+    # naming the y axis 
+    plt.ylabel('GC-ratio') 
+    
+    plt.savefig(output_file)
+    # function to show the plot 
+    plt.show()
